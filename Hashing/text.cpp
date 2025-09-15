b74815c213d7c66d26513650473d0189028d23ac
Meet-510 <mistrymeet941@gmail.com>
2025-09-15 10:12:48 -0600

    Done with basics'

--- Hashing/text.cpp
+++ Hashing/text.cpp
@@ -35,25 +35,53 @@ using namespace std;
 
 // checking the number of occrence in array ( character)
 
-int main(){
-   string s;
-   cin>>s;
-   int q;
-   cin>> q;
+// int main(){
+//    string s;
+//    cin>>s;
+//    int q;
+//    cin>> q;
 
+//    // pre compute 
+//    int hash[26] = {0};
+//    for (int i =0 ; i<s.size();i++){
+//       hash[s[i] -'a']++;
+//    }
+
+//    //taking query characters
+//    while( q--){
+//       char c;
+//       cin>> c;
+//       //fetch pre compute data
+//       cout<<hash[c - 'a']<< endl;
+//    }
+//    return 0;
+// }
+
+
+// map 
+int main (){
+   int n;
+   cin>>n;
+   int arr[n];
+   for ( int i  = 0 ; i < n; i++){
+         cin>> arr[i];
+   }
    // pre compute 
-   int hash[26] = {0};
-   for (int i =0 ; i<s.size();i++){
-      hash[s[i] -'a']++;
+   map<int,int> mpp;
+   for( int i =0; i <n ; i++){
+      mpp[arr[i]]++;
    }
 
-   //taking query characters
-   while( q--){
-      char c;
-      cin>> c;
-      //fetch pre compute data
-      cout<<hash[c - 'a']<< endl;
+
+   int q ;
+   cin>> q;
+   while(q--){
+      int number;
+      cin>> number;
+      // fetch 
+      cout<<mpp[number]<< endl;
+   
+      
    }
    return 0;
 }
-
