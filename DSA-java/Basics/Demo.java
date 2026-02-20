9259d89f50e12705ca74ec59d34ed75f1fda1828
Meet-510 <mistrymeet941@gmail.com>
2026-02-19 21:26:00 -0700

    commit

---
+++ DSA-java/Basics/Demo.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Demo{
+public static void main(String[] args){
+   /* Scanner input = new Scanner(System.in);
+     int rollno = input.nextInt();
+    System.out.println("Your roll number is " + rollno);
+    String name = input.nextLine();
+    System.out.print(name);
+     */
+    /*Scanner input = new Scanner(System.in);
+    int salary = input.nextInt();
+    if(salary >= 25000){
+        salary = salary + 5000;
+    }else {
+        salary = salary + 1000;
+    }
+    System.out.println(salary);
+    */
+
+}
+}
